5fd90368d557451f2c8a3decbe94676cddf2f787
vkovsh <vmukashi7@gmail.com>
2018-06-21 16:43:58 +0300

    d02 ex04

--- day02/ex04/main.cpp
+++ day02/ex04/main.cpp
@@ -1,6 +1,78 @@
 #include "Fixed.class.hpp"
+#include <iostream>
+#include <string>
 
-int main( void )
+int	find_first(std::string expr, char op)
 {
-	return 0;
+	int	brackets = 0;
+	int	index;
+
+	for (index = 0; index < (int)expr.size(); index++)
+	{
+		if (expr[index] == '(')
+			brackets++;
+		else if (expr[index] == ')')
+			brackets--;
+		else if (expr[index] == op && brackets == 0)
+			return index;
+	}
+	return -1;
+}
+
+Fixed	eval_expr(std::string expr)
+{
+	size_t expr_len = expr.size();
+
+	while (expr_len >= 2 && expr[0] == '(' && expr[expr_len - 1] == ')')
+		expr = expr.substr(1, expr_len - 2);
+	if (expr_len == 0)
+	{
+		std::cout << "Invalid format" << std::endl;
+		exit(1);
+	}
+
+	size_t op_index = std::string::npos;
+
+	if ((op_index = find_first(expr, '+')) != -1 && op_index != 0 && op_index != expr_len)
+		return	eval_expr(expr.substr(0, op_index)) +
+				eval_expr(expr.substr(op_index + 1, expr_len - op_index));
+	if ((op_index = find_first(expr, '-')) != -1 && op_index != 0 && op_index != expr_len)
+		return	eval_expr(expr.substr(0, op_index)) -
+				eval_expr(expr.substr(op_index + 1, expr_len - op_index));
+	if ((op_index = find_first(expr, '*')) != -1 && op_index != 0 && op_index != expr_len)
+		return	eval_expr(expr.substr(0, op_index)) *
+				eval_expr(expr.substr(op_index + 1, expr_len - op_index));
+	if ((op_index = find_first(expr, '/')) != -1 && op_index != 0 && op_index != expr_len)
+		return	eval_expr(expr.substr(0, op_index)) /
+				eval_expr(expr.substr(op_index + 1, expr_len - op_index));
+	float value;
+	try
+	{
+		value = std::stof(expr);
+	}
+	catch (const std::exception&)
+	{
+		std::cout << "Invalid format" << std::endl;
+		exit(1);
+	}
+	return Fixed(value);
+}
+
+std::string trim(std::string str)
+{
+	std::string new_str = "";
+	for (size_t i = 0; i < str.size(); ++i)
+		if (str[i] != '\t' && str[i] != ' ')
+			new_str += str[i];
+	return new_str;
+}
+
+int main(int ac, char **av)
+{
+	if (ac >= 2)
+	{
+		std::string expression(av[1]);
+		std::cout << eval_expr(trim(expression)) << std::endl;
+	}
+	return (0);
 }
