db55cece73ab61cffa5ab97c5ab16de73486d583
vkovsh <vmukashi7@gmail.com>
2018-06-28 10:44:21 +0300

    ex02

--- day07/ex02/Array.hpp
+++ day07/ex02/Array.hpp
@@ -1,52 +1,49 @@
-#ifndef ARRAY_HPP
-# define ARRAY_HPP
-//# include <
+#ifndef ARRAY_H
+#define ARRAY_H
+#include <stdexcept>
+#include <iostream>
 
-template <typename T>
-class	Array<T> {
-	private:
-		T				*_arrayPtr;
-		unsigned int	_size;
-
+template< typename T = int>
+class Array
+{
 	public:
-		unsigned int	getSize() {
-			return _size;
+		Array<T>(): _array(NULL), _size(0) {};
+		
+		Array<T>(unsigned int n): _size(n) {
+			_array = (_size) ? new T[n] : 0; 
 		};
-
-		Array<T>() {
-			_size = 0;
-			_arrayPtr = NULL;
+		
+		Array<T>(Array<T> const & src): _array(NULL), _size(0) {
+			*this = src;
 		};
-
-		Array<T>(unsigned int n) {
-			_size = n; 
-			_arrayPtr = new T[_size];
+		
+		~Array<T>() {};
+		
+		unsigned int	size() const {
+			return ( this->_size );
 		};
-
-		~Array<T>() {
-			delete [] _arrayPtr;
-		};
-
-		Array<T>(const Array<T> & cpy) {
-			if (this != &cpy) {
-				delete [] _arrayPtr;
-				_arrayPtr = new T[cpy.getSize()];
-				_size = cpy.getSize();
-				for (unsigned int i = 0; i < getSize(); ++i){
-					_arrayPtr[i] = cpy[i];
+		
+		Array<T> &	operator=( Array<T> const & rhs ) {
+			if ( this != &rhs ) {
+				if (_array) {
+					delete [] _array;
 				}
+				this->_size = rhs._size;
+				this->_array = new T[rhs._size];
+				std::copy(rhs._array, rhs._array + rhs.size(), _array);
 			}
-
-		};
-
-		Array<T>	&operator = (const Array<T> & cpy) {
-			*this = cpy;
 			return *this;
-		}
-
-		T			&operator [] (const unsigned int index) {
-			return _arrayPtr[index];
 		};
+		
+		T &	operator[]( unsigned int num ) {
+			if (num < this->_size)
+				return (this->_array[num]);
+			else
+				throw std::range_error("class Array: Out of bound!");
+		};
+	
+	private:
+		T*				_array;
+		unsigned int	_size;
 };
-
 #endif
---
+++ day07/ex02/main.cpp
@@ -0,0 +1,28 @@
+#include "Array.hpp"
+#include <cstdlib>
+
+int	main(void)
+{
+	unsigned int i = 9;
+	Array<int> intNum = Array<int>(i);
+	for (unsigned int j = 0; j < intNum.size(); ++j) {
+		intNum[j] = j;
+	}
+	for (unsigned int j = 0; j < intNum.size(); ++j) {
+		std::cout << intNum[j] << std::endl;
+	}
+	try {
+		intNum[9] = 42;
+	}
+	catch (std::range_error e){
+		std::cout << e.what() << std::endl;
+	}
+	Array<int> intNum2 = Array<int>(10);
+	for (unsigned int j = 0; j < intNum2.size(); ++j) {
+		intNum2[j] = j;
+	}
+	intNum = intNum2;
+	for (unsigned int j = 0; j < intNum.size(); ++j) {
+		std::cout << intNum[j] << std::endl;
+	}
+}
